16be81eaa4e0f4da6e1a7514457397267de260aa
Tayhan87 <tayhanhossain87@gmail.com>
2024-06-21 12:03:25 +0600

    updated

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
 const random = Math.floor(Math.random() * 10 + 1);
 console.log(random);
-let i = 0;
-document.querySelector("button").addEventListener("click", goback);
+let i = 1;
 
+document.querySelector("button").addEventListener("click", goback);
 
 function goback(e) {
     e.preventDefault();
     document.querySelector("button").remove();
     createbutton();
-    let flag = false;
-
+    i = 1;
     document.querySelector(".submit").addEventListener("click", () => {
         let val = document.querySelector(".get").value;
-        if (val == random)
-        {
-            alert("Congratuation!Right Guess");
+        if (val == random) {
+            alert("Congratulations! Right Guess");
             alert("You win");
             location.reload(true);
-        }
-            
-
-        else if (val < random)
+        } else if (val < random) {
             alert("Correct answer is greater.");
-        else
+        } else {
             alert("Correct answer is smaller.");
+        }
 
-
-         if(val != random && i > 3)
+        if (val != random && i > 2) {
             alert("You lose");
+            location.reload(true); 
+        }
         i++;
-
     });
-
 }
-
-
 
 function createbutton() {
     let input = document.createElement('input');
     let submit = document.createElement('input');
-    submit.className = "submit mt-3 mb-5 ";
+    submit.className = "submit mt-3 mb-5";
     input.className = "get";
     submit.value = "Guess";
     submit.type = "submit";
@@ -48,5 +41,4 @@ function createbutton() {
     container.classList.add("bg-info");
     container.appendChild(input);
     container.appendChild(submit);
-
 }
